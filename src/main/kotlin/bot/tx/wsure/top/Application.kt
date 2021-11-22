0609786c1a523796ffcf2f01ae1b22864988e8ae
Wsure <root@wsure.top>
2021-11-23 00:49:56 +0800

    add room

--- src/main/kotlin/bot/tx/wsure/top/Application.kt
+++ src/main/kotlin/bot/tx/wsure/top/Application.kt
@@ -4,10 +4,10 @@ import bot.tx.wsure.top.bililiver.BiliLiverConsole
 import bot.tx.wsure.top.bililiver.BiliLiverEvent
 import bot.tx.wsure.top.bililiver.dtos.api.room.Room
 import bot.tx.wsure.top.component.bililiver.SuperChatNotify
-import bot.tx.wsure.top.config.Global.token
-import bot.tx.wsure.top.official.OfficialBotClient
 import bot.tx.wsure.top.component.official.EditRoles
 import bot.tx.wsure.top.component.unofficial.YbbTrain
+import bot.tx.wsure.top.config.Global.token
+import bot.tx.wsure.top.official.OfficialBotClient
 import bot.tx.wsure.top.plugins.*
 import bot.tx.wsure.top.unofficial.UnOfficialBotClient
 import io.ktor.server.engine.*
@@ -25,30 +25,41 @@ fun main() {
         configureSerialization()
         configureSockets()
 
-        val scConfig = mutableMapOf<String,List<Pair<Long,Long>>>(
-            "21452505" to listOf(6000051636714649 to 1370732), // 七海 七海频道直播讨论子频
+        val scConfig = mutableMapOf<String, List<Pair<Long, Long>>>(
+            "21452505" to listOf(
+                6000051636714649 to 1370732,// 七海 七海频道直播讨论子频
+                36667731636792997 to 1384516,// 七海  阿梓七海子频
+            ),
+            "510" to listOf(
+                6000051636714649 to 1454836,// 阿梓 七海频道 火车站子频
+                36667731636792997 to 1365148,// 阿梓 阿梓 直播讨论
+            ),
             "213" to listOf(6000051636714649 to 1454836), // C酱 七海频道 火车站子频
-            "876396" to listOf(6000051636714649 to 1454836), // 忘了是谁了  七海频道直播讨论子频
-            "22047448" to listOf(6000051636714649 to 1454836), // 星宮汐Official 七海频道直播讨论子频
-            "21402309" to listOf(6000051636714649 to 1454836), // 眞白花音_Official 七海频道直播讨论子频
-            "14327465" to listOf(6000051636714649 to 1454836), // 花园Serena 七海频道直播讨论子频
+            "876396" to listOf(6000051636714649 to 1454836), // 忘了是谁了  七海频道 火车站子频
+            "22047448" to listOf(6000051636714649 to 1454836), // 星宮汐Official 七海频道 火车站子频
+            "21402309" to listOf(6000051636714649 to 1454836), // 眞白花音_Official 七海频道 火车站子频
+            "14327465" to listOf(6000051636714649 to 1454836), // 花园Serena 七海频道 火车站子频
         )
 
-        val unOfficialBotClient = UnOfficialBotClient( listOf(
-            YbbTrain(),
+        val unOfficialBotClient = UnOfficialBotClient(
+            listOf(
+                YbbTrain(),
 //            SendRoles()
-        ))
+            )
+        )
         val useBL = true
-        if(useBL){
+        if (useBL) {
             scConfig.onEach { room ->
-                BiliLiverConsole(room.key){ initBiliLiverEvents(it,unOfficialBotClient,room.value) }
+                BiliLiverConsole(room.key) { initBiliLiverEvents(it, unOfficialBotClient, room.value) }
             }
 
         }
 
-        val botClient = OfficialBotClient(token, listOf(
-            EditRoles(unOfficialBotClient)
-        ))
+        val botClient = OfficialBotClient(
+            token, listOf(
+                EditRoles(unOfficialBotClient)
+            )
+        )
 
         this.attributes.put(AttributeKey("officeBot"), botClient)
 
@@ -56,8 +67,13 @@ fun main() {
 
 
 }
-fun initBiliLiverEvents(room: Room,unOfficialBotClient: UnOfficialBotClient,guildAndChannel:List<Pair<Long,Long>>):List<BiliLiverEvent>{
+
+fun initBiliLiverEvents(
+    room: Room,
+    unOfficialBotClient: UnOfficialBotClient,
+    guildAndChannel: List<Pair<Long, Long>>
+): List<BiliLiverEvent> {
     return mutableListOf(
-        SuperChatNotify(guildAndChannel,room,unOfficialBotClient)
+        SuperChatNotify(guildAndChannel, room, unOfficialBotClient)
     )
 }
